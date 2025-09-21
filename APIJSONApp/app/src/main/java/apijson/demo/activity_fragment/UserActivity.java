6bc06d5d7a593bbb2a53e674235ce6497bcc4d5c
TommyLemon <tommylemon@qq.com>
2025-09-21 16:31:53 +0800

    把 FloatWindow 的 gradle 一远程依赖改成源码依赖来解决 Android 14+ 注册广播报错少传 flag RECEIVER_EXPORTED/RECEIVER_NOT_EXPORTED

--- APIJSONApp/app/src/main/java/apijson/demo/activity_fragment/UserActivity.java
+++ APIJSONApp/app/src/main/java/apijson/demo/activity_fragment/UserActivity.java
@@ -20,7 +20,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.widget.MenuPopupWindow;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
